8560c5405c8ff91eb3049054f0d6160bf548ecdb
tom_glb <goodMorning_glb@atguigu.com>
2020-07-08 17:41:50 +0800

    测试分支

--- ly-auth/ly-auth-common/src/main/java/com/leyou/entity/JwtConstans.java
+++ ly-auth/ly-auth-common/src/main/java/com/leyou/entity/JwtConstans.java
@@ -1,5 +1,8 @@
 package com.leyou.entity;
 
+/**
+ * 静态常量
+ */
 public abstract class JwtConstans {
     public static final String JWT_KEY_ID = "id";
     public static final String JWT_KEY_USER_NAME = "username";
